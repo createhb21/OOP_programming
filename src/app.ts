6dbfe965b925d520660e04317f91df70742c137e
Createhb21 <alsghk9701@gmail.com>
2021-12-04 02:42:10 +0900

    adding PageItemComponent

--- src/app.ts
+++ src/app.ts
@@ -2,25 +2,26 @@ import { ImageComponent } from './components/page/item/image.js';
 import { VideoComponent } from './components/page/item/video.js';
 import { NoteComponent } from './components/page/item/note.js';
 import { ToDoComponent } from './components/page/item/todo.js';
-import { PageComponent } from './components/page/page.js';
+import { PageComponent, Composable } from './components/page/page.js';
+import { Component } from './components/component.js';
 
 class App {
-    private readonly page: PageComponent;
+    private readonly page: Component & Composable;
     constructor(appRoot: HTMLElement) {
         this.page = new PageComponent();
         this.page.attachTo(appRoot);
 
         const image = new ImageComponent('Image Title', 'https://picsum.photos/600/300');
-        image.attachTo(appRoot, 'beforeend');
+        this.page.addChild(image);
 
         const video = new VideoComponent('Video Title', 'https://www.youtube.com/watch?v=dz-3A9Sz7O4');
-        video.attachTo(appRoot, 'beforeend');
+        this.page.addChild(video);
 
         const note = new NoteComponent('Note Title', 'Note Body');
-        note.attachTo(appRoot, 'beforeend');
+        this.page.addChild(note);
 
         const todo = new ToDoComponent('Todo Title', 'Todo Item');
-        todo.attachTo(appRoot, 'beforeend');
+        this.page.addChild(todo);
     }
 }
 
