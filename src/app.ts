720b404131c632b74803497ffe3c86626a5c6445
Createhb21 <alsghk9701@gmail.com>
2021-12-04 15:30:02 +0900

    Wire up everything together in Dialog

--- src/app.ts
+++ src/app.ts
@@ -5,15 +5,13 @@ import { ToDoComponent } from './components/page/item/todo.js';
 import { PageComponent, Composable, PageItemComponent } from './components/page/page.js';
 import { Component } from './components/component.js';
 import { InputDialog } from './components/dialog/dialog.js';
+import { MediaSectionInput } from './components/dialog/input/media-input.js';
 
 class App {
     private readonly page: Component & Composable;
-    constructor(appRoot: HTMLElement) {
+    constructor(appRoot: HTMLElement, dialogRoot: HTMLElement) {
         this.page = new PageComponent(PageItemComponent);
         this.page.attachTo(appRoot);
-
-        const image = new ImageComponent('Image Title', 'https://picsum.photos/600/300');
-        this.page.addChild(image);
 
         const video = new VideoComponent('Video Title', 'https://www.youtube.com/watch?v=dz-3A9Sz7O4');
         this.page.addChild(video);
@@ -27,18 +25,20 @@ class App {
         const imageBtn = document.querySelector('#new-image')! as HTMLButtonElement;
         imageBtn.addEventListener('click', () => {
             const dialog = new InputDialog();
+            const mediaSection = new MediaSectionInput();
+            dialog.addChild(mediaSection);
+            dialog.attachTo(dialogRoot);
 
             dialog.setOnCloseListner(() => {
-                dialog.removeFrom(document.body);
+                dialog.removeFrom(dialogRoot);
             });
             dialog.setOnSubmitListner(() => {
-                // 섹션을 만들어서 페이지에 추가 해준다
-                dialog.removeFrom(document.body);
+                const image = new ImageComponent(mediaSection.title, mediaSection.url);
+                this.page.addChild(image);
+                dialog.removeFrom(dialogRoot);
             });
-
-            dialog.attachTo(document.body);
         });
     }
 }
 
-new App(document.querySelector('.document')! as HTMLElement);
+new App(document.querySelector('.document')! as HTMLElement, document.body);
